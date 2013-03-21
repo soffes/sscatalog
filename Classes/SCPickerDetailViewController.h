1481cdc69bfeab4395b0b699c434a097f0d1fdc2
Sam Soffes <sam@soff.es>
2013-03-21 09:32:56 -0400

    Modernize

--- Classes/SCPickerDetailViewController.h
+++ Classes/SCPickerDetailViewController.h
@@ -3,7 +3,7 @@
 //  SSCatalog
 //
 //  Created by Sam Soffes on 10/9/09.
-//  Copyright 2009 Sam Soffes, Inc. All rights reserved.
+//  Copyright 2009-2013 Sam Soffes, Inc. All rights reserved.
 //
 
 @interface SCPickerDetailViewController : SSPickerViewController
